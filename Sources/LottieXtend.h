fa238234741e0335f0a0db0b4c1affff455b3b53
iDonJose <donor.develop@gmail.com>
2018-11-21 19:45:31 +0100

    Renaming in umbrella

--- Sources/LottieXtend.h
+++ Sources/LottieXtend.h
@@ -1,6 +1,6 @@
 //
-//  ReactiveSwifty.h
-//  ReactiveSwifty
+//  LottieXtend.h
+//  LottieXtend
 //
 //  Created by José Donor on 10/11/2018.
 //  Copyright © 2018 iDonJose. All rights reserved.
@@ -10,12 +10,12 @@
 
 #import <Swifty/Swifty.h>
 
-//! Project version number for ReactiveSwifty.
-FOUNDATION_EXPORT double ReactiveSwiftyVersionNumber;
+//! Project version number for LottieXtend.
+FOUNDATION_EXPORT double LottieXtendVersionNumber;
 
-//! Project version string for ReactiveSwifty.
-FOUNDATION_EXPORT const unsigned char ReactiveSwiftyVersionString[];
+//! Project version string for LottieXtend.
+FOUNDATION_EXPORT const unsigned char LottieXtendVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ReactiveSwifty/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <LottieXtend/PublicHeader.h>
 
 
